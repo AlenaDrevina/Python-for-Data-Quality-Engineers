1685bde03e001f92b04a24fa8c86f7c7e44c2269
Alena_Drevina <alena_drevina@epam.com>
2023-03-01 08:22:09 +0100

    Module_4 changed

--- HW_module_4_task2.py
+++ HW_module_4_task2.py
@@ -2,19 +2,20 @@ import string
 import random
 
 # create a list of random number of dicts (from 2 to 10)
-def create_list_of_dict(a, b, c=4, e=5, f=1):
-    dict_List = []
-    for x in range(random.randint(a, b)):
-        size = c
+def create_list_of_dict(random_number_from, random_number_to, size_dictionary=4, random_letter_from = 5, random_letter_to = 1):
+    dict_list = []
+    for x in range(random.randint(random_number_from, random_number_to)):
+        size = size_dictionary
         keys = random.sample(string.ascii_lowercase, size)
-        values = (random.randint(f, e) for y in range(size))
-        one_Dict = dict(zip(keys, values))
+        values = (random.randint(random_letter_from, random_letter_to) for y in range(size))
+        one_dict = dict(zip(keys, values))
 
-        dict_List.append(one_Dict)
-    return dict_List
+        dict_list.append(one_dict)
+    return dict_list
 
-dict_List = create_list_of_dict(a=2, b=10, e=50)               # call the function with parameters
-print('Initial list of dictionaries: ', dict_List)
+# call the function with parameters
+dict_list = create_list_of_dict(random_number_from = 2, random_number_to = 10, random_letter_to = 50)
+print('Initial list of dictionaries: ', dict_list)
 
 # get previously generated list of dicts and create one common dict
 # create the function with 1 parameter for dict_List
@@ -22,7 +23,7 @@ def create_lists_with_keys(lst):
     distinct_keys = []
     duplicated_keys = []
 
-    for z in dict_List:
+    for z in dict_list:
         for key in z:
              if key not in distinct_keys:
                  distinct_keys.append(key)
@@ -32,7 +33,8 @@ def create_lists_with_keys(lst):
     duplicated_keys.sort()
     return distinct_keys, duplicated_keys
 
-distinct_keys, duplicated_keys = create_lists_with_keys(dict_List)  # call the function create_list_of_dict with parameter dict_List
+# call the function create_list_of_dict with parameter dict_list
+distinct_keys, duplicated_keys = create_lists_with_keys(dict_list)
 
 # create the function with 3 parameters for distinct_keys, duplicated_keys, list_of_dict
 def create_one_common_dict(dist_k, dupl_k, lst):
@@ -41,7 +43,7 @@ def create_one_common_dict(dist_k, dupl_k, lst):
         if key in duplicated_keys:
             max_value = 0
             dict_number = 0
-            for i, rand_dict in enumerate(dict_List):
+            for i, rand_dict in enumerate(dict_list):
                 if key in rand_dict and rand_dict[key] > max_value:
 
                     max_value = rand_dict[key]
@@ -49,11 +51,11 @@ def create_one_common_dict(dist_k, dupl_k, lst):
             key_final = key + '_' + str(dict_number)
             one_common_dict.setdefault(key_final, max_value)
         if key not in duplicated_keys:
-            for rand_dict in dict_List:
+            for rand_dict in dict_list:
                 if key in rand_dict:
                     one_common_dict.setdefault(key, rand_dict[key])
     return one_common_dict
 
 # call the function with 3 parameters
-one_common_dict_final = create_one_common_dict(distinct_keys, duplicated_keys, dict_List)
+one_common_dict_final = create_one_common_dict(distinct_keys, duplicated_keys, dict_list)
 print('Common dictionary: ', one_common_dict_final)  # show final sorted common dictionary
--- HW_module_4_task3.py
+++ HW_module_4_task3.py
@@ -1,38 +1,36 @@
 import re
 
+# function which corrects text format
+def text_correct(message):
+    text = message.lower()                            # converts a message into lower case
+    splitted = re.split('(\.\s+)', text)              # split text on sentence
+    capital_letters = [word.capitalize() for word in splitted]  # capitalize first letters of each sentence
+    capital_letters_join = "".join(capital_letters)            # union all parts
+    misspelling = capital_letters_join.replace(' iz', ' is')   # replace 'iz' by 'is'
+    return misspelling
+
+# function which returns last word and create new sentence
+def last_word(message):
+    find_all_last_word = re.findall(r"\s(\w+)\.\s+", text_correct(message))  # find all last words
+    all_words = ', '.join(find_all_last_word).lower().capitalize()           # union them
+    sentence = f'\t{all_words}.\n'  # create new sentence
+    return sentence
+
+# function which unions correct text and sentence that contains last words
+def new_text(message):
+    text = text_correct(message) + last_word(message)
+    return text
+
+def filtered_whitesp(message):
+    return len(list(s for s in message if s.isspace()))
+
 # Text variable
 message = """homEwork:
-	tHis iz your homeWork, copy these Text to variable.
+	tHis iz your homeWork, copy these Text to variable. 
 	You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.
-	it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE.
+	it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE. 
 	last iz TO calculate nuMber OF Whitespace characteRS in this Text. caREFULL, not only Spaces, but ALL whitespaces. I got 87.
 """
 
-# Last words of each existing sentence
-def find_all_last_word(a, b):
-    return re.findall(a, b)
-
-
-# Additional activities concerns with normalization of our text
-def splitted(a):
-    text = message + ', '.join(find_all_last_word)
-    splitted = re.split(a, text)
-    capital_letters = [i.capitalize() for i in splitted]
-    return ''.join(capital_letters)
-
-# Replace iz by is if necessary
-def misspelling(a, b):
-    return splitted.replace(a, b)
-
-# Calculate number of whitespace characters in this text
-def filtered_whitesp():
-    return len(list(s for s in misspelling if s.isspace()))
-
-find_all_last_word = find_all_last_word('\s(\w+)?\.', message)
-
-splitted = splitted('(\.\s+)')
-
-misspelling = misspelling(" iz", " is")
-print(f'Corrected mistakes:\n{misspelling}')
-
-print(f'Number of whitespaces: {filtered_whitesp()}')
+print(f'Corrected mistakes:\n', new_text(message))
+print(f'Number of whitespaces: {filtered_whitesp(message)}')
